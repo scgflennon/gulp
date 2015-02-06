0cc8493b1b65d05827a8f22ed750d1f465baffcc
萩原航 <hagiharawataru@hagiharawataru-no-iMac.local>
2015-02-06 15:07:53 +0900

    最新版に差し替え

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,3 @@
-'use strict';
 var gulp = require('gulp');
 var runSequence = require('run-sequence'); //タスクを並列/直列処理する
 var compass = require('gulp-compass'); //Compass
@@ -7,7 +6,8 @@ var notify = require("gulp-notify"); //デスクトップ通知
 var size = require('gulp-size'); // 制作物のサイズ確認
 var imagemin = require('gulp-imagemin'); // 画像圧縮
 var pngquant = require('imagemin-pngquant'); // PNG画像圧縮
-var gzip = require("gulp-gzip"); // gizp圧縮
+var uglify = require('gulp-uglify'); // js圧縮
+var gzip = require('gulp-gzip');//gizp圧縮
 var changed  = require('gulp-changed'); // 変更箇所のみ
 var frontnote = require("gulp-frontnote"); // テンプレートエンジン
 
@@ -18,16 +18,18 @@ gulp.task('compass', function(){
 	.pipe(plumber({
 		errorHandler: notify.onError("Error: <%= error.message %>")
 	}))
-	.pipe(frontnote({
-		css: '../css/**.css',
-		out: './StyleGuide'
-	}))
 	.pipe(compass({
     	config_file: 'config.rb',
     	comments: false,
     	css: 'css/',
     	sass: 'scss/'
-	}))
+	}));
+});
+
+//CSSのgzip圧縮
+gulp.task('cssgzip',function(){
+	var s = size();
+	gulp.src('css/**/*.css')
 	.pipe(gzip())
 	.pipe(gulp.dest('css'))
 	.pipe(s)
@@ -39,6 +41,39 @@ gulp.task('compass', function(){
 	}));
 });
 
+//jsのgzip圧縮
+gulp.task('jsgzip',function(){
+	var s = size();
+	gulp.src('js/min/**/*.js')
+	.pipe(gzip())
+	.pipe(gulp.dest('js/min/'))
+	.pipe(s)
+	.pipe(notify({
+		onLast: true,
+		message: function () {
+			return 'Total CSS size (gziped)' + s.prettySize;
+		}
+	}));
+});
+
+//js圧縮
+gulp.task('js', function() {
+  gulp.src(['js/**/*.js','!js/min/**/*.js'])
+    .pipe(uglify())
+    .pipe(gulp.dest('js/min'));
+});
+
+//スタイルシート生成
+gulp.task('styleguide', function(){
+	gulp.src('scss/**/*.scss')
+	.pipe(frontnote({
+		clean: true,
+		css: '../css/stylesheet.css',
+		out: './StyleGuide'
+	}));
+});
+
+//画像圧縮
 gulp.task('imagemin', function () {
 	var s = size();
 	return gulp.src('images/*.+(jpg|jpeg|png|gif|svg)')
@@ -57,23 +92,26 @@ gulp.task('imagemin', function () {
 	}));
 });
 
+//監視
 gulp.task('watch',function(){
-	gulp.watch(["js/**/*.js","!js/min/**/*.js"],["js"]);
-	gulp.watch(['scss/**/*.scss'],['compass']);
+	gulp.watch(["js/**/*.js","!js/min/**/*.js"],["js",'jsgzip']);
+	gulp.watch(['scss/**/*.scss'],['compass','styleguide','cssgzip']);
 });
 
+//デフォルト設定
 gulp.task('default', function(callback) {
 	return runSequence(
-	'compass',
-	['watch'],
-	callback
+		'compass',
+		['watch'],
+		callback
 	);
 });
 
+//完成版作成
 gulp.task('prod', function(callback){
 	return runSequence(
-	['compass'],
-	['imagemin'],
-	callback
+		['compass','js'],
+		['imagemin','styleguide','cssgzip','jsgzip'],
+		callback
 	);
 });
